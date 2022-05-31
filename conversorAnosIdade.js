ac0ee5883f5654e167a57c602f6d52c239dc2737
Thiago Souza <102497892+ThiagoSouzaC@users.noreply.github.com>
2022-05-30 22:34:44 -0300

    Add files via upload

---
+++ conversorAnosIdade.js
@@ -0,0 +1,7 @@
+
+function converterIdadeEmAnosParaDias(idade) { 
+    const idadeEmDias = idade*365
+    return idadeEmDias
+}
+
+console.log('Você tem:', converterIdadeEmAnosParaDias(25), 'dias de vida.')
